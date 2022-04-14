209c025a922d028aa1ffacccca9b1f75de423d72
Pawel Niewiadowski <pw.niewiadowski@gmail.com>
2022-04-14 21:10:35 +0200

    Add listen layout

--- src/scenes/listen.tsx
+++ src/scenes/listen.tsx
@@ -1,12 +1,85 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {IconButton} from 'react-native-paper';
+import {Bar} from 'react-native-progress';
+import {COLOR_PRIMARY, COLOR_SECONDARY_TEXT} from '../styles/colors';
 
 function ListenScreen() {
+  const [progress, setProgress] = React.useState(0.4);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Listen Screen</Text>
+    <View style={{flex: 1}}>
+      <View style={[styles.box, styles.boxTop]}>
+        <View style={[styles.box, styles.boxTopTop]}>
+          <Text>TOP</Text>
+        </View>
+        <View style={[styles.box, styles.boxTopBottom]}>
+          <Bar progress={progress} width={null} style={styles.progress} />
+        </View>
+      </View>
+      <View style={[styles.box, styles.boxBottom]}>
+        <View style={[styles.box, styles.boxBottom.element]}>
+          <IconButton
+            icon={require('../assets/icons/skip_prev_48.png')}
+            size={92}
+            color={COLOR_PRIMARY}
+            style={styles.button}
+          />
+        </View>
+        <View style={[styles.box, styles.boxBottom.element]}>
+          <IconButton
+            icon={require('../assets/icons/play_48.png')}
+            size={92}
+            color={COLOR_PRIMARY}
+            style={styles.button}
+          />
+        </View>
+        <View style={[styles.box, styles.boxBottom.element]}>
+          <IconButton
+            icon={require('../assets/icons/skip_next_48.png')}
+            size={92}
+            color={COLOR_PRIMARY}
+            style={styles.button}
+          />
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  box: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  boxBottom: {
+    flex: 2,
+    flexDirection: 'row',
+    element: {
+      flex: 1,
+      height: '100%',
+    },
+  },
+  boxTop: {
+    flex: 3,
+  },
+  boxTopTop: {
+    flex: 4,
+  },
+  boxTopBottom: {
+    flex: 1,
+  },
+  button: {
+    width: '100%',
+    height: '100%',
+  },
+  progress: {
+    width: '80%',
+  },
+});
 
 export default ListenScreen;
